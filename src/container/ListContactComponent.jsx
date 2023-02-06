60123aa70af4ab5b80de500af73a8a2978222134
Juan David Castro <juamcaza@gmail.com>
2023-02-06 10:33:15 -0500

    second commit

--- src/container/ListContactComponent.jsx
+++ src/container/ListContactComponent.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import  { React, useState } from 'react'
 import ContactComponent from '../components/pure/ContactComponent';
 import { Contact } from '../models/contact.class';
 import { STATE } from '../models/state.enum';
 
 function ListContactComponent() {
 
-    const defaultContact = new Contact("Juan", "David", "example@email.com", STATE.DISCONNECTED)
+    const [state, setState] = useState(STATE.DISCONNECTED);
+
+    const stateChange = ()=>{
+        setState( state ? STATE.DISCONNECTED : STATE.CONNECTED);
+    }
+
+    const defaultContact = new Contact("Juan", "David", "example@email.com", state)
 
   return (
     <div>
         <h1>Contactos:</h1>
         <ContactComponent contact={defaultContact}/>
+        <button onClick={stateChange}>Cambiar estado</button>
     </div>
   )
 }
